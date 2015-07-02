16132834d011c15fb5b87a9b22f4f9bc8df9584e
billcrosbie <crosbie.git@gmail.com>
2015-07-02 17:30:52 -0400

    Add XML parse to Deck pp 569-571

--- Assets/__Scripts/Deck.cs
+++ Assets/__Scripts/Deck.cs
@@ -6,7 +6,14 @@ public class Deck : MonoBehaviour {
 
 	public bool _____________________;
 
-	public PT_XMLReader		xmlr;
+	public PT_XMLReader					xmlr;
+	// add from p 569
+	public List<string>					cardNames;
+	public List<Card>					cards;
+	public List<Decorator>				decorators;
+	public List<CardDefinition>			cardDefs;
+	public Transform					deckAnchor;
+	public Dictionary<string, Sprite>	dictSuites;
 
 
 	// called by Prospector when it is ready
@@ -28,8 +35,59 @@ public class Deck : MonoBehaviour {
 		s += " y=" + xmlr.xml ["xml"] [0] ["decorator"] [0].att ("y");
 		s += " scale=" + xmlr.xml ["xml"] [0] ["decorator"] [0].att ("scale");
 		print (s);
-	}
-
-
-
-}
+		
+		//Read decorators for all cards
+		// these are the small numbers/suits in the corners
+		decorators = new List<Decorator>();
+		// grab all decorators from the XML file
+		PT_XMLHashList xDecos = xmlr.xml["xml"][0]["decorator"];
+		Decorator deco;
+		for (int i=0; i<xDecos.Count; i++) {
+			// for each decorator in the XML, copy attributes and set up location and flip if needed
+			deco = new Decorator();
+			deco.type = xDecos[i].att ("type");
+			deco.flip = (xDecos[i].att ("flip") == "1");   // too cute by half - if it's 1, set to 1, else set to 0
+			deco.scale = float.Parse (xDecos[i].att("scale"));
+			deco.loc.x = float.Parse (xDecos[i].att("x"));
+			deco.loc.y = float.Parse (xDecos[i].att("y"));
+			deco.loc.z = float.Parse (xDecos[i].att("z"));
+			decorators.Add (deco);
+		}
+		
+		// read pip locations for each card rank
+		// read the card definitions, parse attribute values for pips
+		cardDefs = new List<CardDefinition>();
+		PT_XMLHashList xCardDefs = xmlr.xml["xml"][0]["card"];
+		
+		for (int i=0; i<xCardDefs.Count; i++) {
+			// for each carddef in the XML, copy attributes and set up in cDef
+			CardDefinition cDef = new CardDefinition();
+			cDef.rank = int.Parse(xCardDefs[i].att("rank"));
+			
+			PT_XMLHashList xPips = xCardDefs[i]["pip"];
+			if (xPips != null) {			
+				for (int j = 0; j < xPips.Count; j++) {
+					deco = new Decorator();
+					deco.type = "pip";
+					deco.flip = (xPips[j].att ("flip") == "1");   // too cute by half - if it's 1, set to 1, else set to 0
+					
+					deco.loc.x = float.Parse (xPips[j].att("x"));
+					deco.loc.y = float.Parse (xPips[j].att("y"));
+					deco.loc.z = float.Parse (xPips[j].att("z"));
+					if(xPips[j].HasAtt("scale") ) {
+						deco.scale = float.Parse (xPips[j].att("scale"));
+					}
+					cDef.pips.Add (deco);
+				} // for j
+			}// if xPips
+			
+			// if it's a face card, map the proper sprite
+			// foramt is ##A, where ## in 11, 12, 13 and A is letter indicating suit
+			if (xCardDefs[i].HasAtt("face")){
+				cDef.face = xCardDefs[i].att ("face");
+			}
+			cardDefs.Add (cDef);
+		} // for i < xCardDefs.Count
+	} // ReadDeck
+	
+} // Deck class
