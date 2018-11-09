734e5f180d41f59958d22440b365acfecd248dc5
zephyrzambrano <43213451+zephyrzambrano@users.noreply.github.com>
2018-11-09 12:06:29 -0500

    Laying Out the Mine Tableau

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -10,19 +10,24 @@ public class Prospector : MonoBehaviour {
 	static public Prospector 	S;
 
 	[Header("Set in Inspector")]
-	public TextAsset			deckXML;
-
+	public TextAsset deckXML;
+    public TextAsset layoutXML;
 
 	[Header("Set Dynamically")]
-	public Deck					deck;
+	public Deck	deck;
+    public Layout layout;
 
 	void Awake(){
-		S = this;
+		S = this; // Set up a Singleton for Prospector
 	}
 
 	void Start() {
-		deck = GetComponent<Deck> ();
-		deck.InitDeck (deckXML.text);
+		deck = GetComponent<Deck> (); // Get the deck
+		deck.InitDeck (deckXML.text); // Pass DeckXML
+        Deck.Shuffle(ref deck.cards); // This shuffles the deck
+
+        layout = GetComponent<Layout>(); // Get the Layout component
+        layout.ReadLayout(layoutXML.text); // Pass LayoutXML to it
 	}
 
 }
