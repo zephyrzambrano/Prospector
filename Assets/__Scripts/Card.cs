b294d77ba7177c69bfba1683c2e4ffc040627776
zephyrzambrano <43213451+zephyrzambrano@users.noreply.github.com>
2018-11-09 18:51:15 -0500

    Setting Up Sorting Layers

--- Assets/__Scripts/Card.cs
+++ Assets/__Scripts/Card.cs
@@ -15,6 +15,55 @@ public class Card : MonoBehaviour {
 	public GameObject back;  // back of card;
 	public CardDefinition def;  // from DeckXML.xml		
 
+    public SpriteRenderer[] spriteRenderers;
+
+    void Start() {
+        SetSortOrder(0); // Ensures that the card starts properly depth sorted
+    }
+
+    // If spriteRenderers is not yet defined, this function defines it
+    public void PopulateSpriteRenderers() {
+        // If spriteRenderers is null or empty
+        if (spriteRenderers == null || spriteRenderers.Length == 0) {
+            // Get SpriteRenderer Components of this GameObject and its children
+            spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        }
+    }
+
+    public void SetSortingLayerName(string tSLN) {
+        PopulateSpriteRenderers();
+
+        foreach (SpriteRenderer tSR in spriteRenderers) {
+            tSR.sortingLayerName = tSLN;
+        }
+    }
+
+    // sets the sortingOrder of all SpriteRenderer Components
+    public void SetSortOrder(int sOrd) {
+        PopulateSpriteRenderers();
+
+        // Iterate through all the spriteRenderers as tSR
+        foreach (SpriteRenderer tSR in spriteRenderers) {
+            if (tSR.gameObject == this.gameObject) {
+                // If the gameObject is this.gameObject, it's the background
+                tSR.sortingOrder = sOrd; // Set it's order to sOrd
+                continue; // And continue to the next iteration of the loop
+            }
+
+            // Each of the children of this GameObject are named
+            // switch based on the name
+            switch (tSR.gameObject.name) {
+                case "back": // If the name is "back
+                    tSR.sortingOrder = sOrd + 2;
+                    break;
+                case "face": // If the name is "face"
+                default: // Or if it's anything else
+                    // Set it to the middle layer to be above the background
+                    tSR.sortingOrder = sOrd + 1;
+                    break;
+            }
+        }
+    }
 
 	public bool faceUp {
 		get {
@@ -25,17 +74,7 @@ public class Card : MonoBehaviour {
 			back.SetActive(!value);
 		}
 	}
-
-
-	// Use this for initialization
-	void Start () {
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 } // class Card
 
 [System.Serializable]
