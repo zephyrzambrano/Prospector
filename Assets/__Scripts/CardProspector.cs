b9e23dee9b3244dae6740394e800bdc7f7c8a7ff
zephyrzambrano <43213451+zephyrzambrano@users.noreply.github.com>
2018-11-09 18:01:58 -0500

    Working with CardProspector - A Subclass of Card

---
+++ Assets/__Scripts/CardProspector.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// An enum defines a variable type with a few prenamed values
+public enum eCardState {
+    drawpile,
+    tableau,
+    target,
+    discard
+}
+
+public class CardProspector : Card { // Make sure CardProspector extends Card
+
+    [Header("Set Dynamically: CardProspector")]
+    // This is how you use the enum eCardState
+    public eCardState state = eCardState.drawpile;
+    // The hiddenBy list stores which other cards will keep this one face down
+    public List<CardProspector> hiddenBy = new List<CardProspector>();
+    // The layoutID matches this card to the tableau XML if it's a tableau card
+    public int layoutID;
+    // The SlotDef class stores information pulled in from the LayoutXML <slot>
+    public SlotDef slotDef;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 
 public class Prospector : MonoBehaviour {
 
-	static public Prospector 	S;
+	static public Prospector S;
 
 	[Header("Set in Inspector")]
 	public TextAsset deckXML;
@@ -16,18 +16,29 @@ public class Prospector : MonoBehaviour {
 	[Header("Set Dynamically")]
 	public Deck	deck;
     public Layout layout;
+    public List<CardProspector> drawPile;
 
 	void Awake(){
 		S = this; // Set up a Singleton for Prospector
 	}
 
-	void Start() {
-		deck = GetComponent<Deck> (); // Get the deck
-		deck.InitDeck (deckXML.text); // Pass DeckXML
+    void Start() {
+        deck = GetComponent<Deck>(); // Get the deck
+        deck.InitDeck(deckXML.text); // Pass DeckXML
         Deck.Shuffle(ref deck.cards); // This shuffles the deck
 
         layout = GetComponent<Layout>(); // Get the Layout component
         layout.ReadLayout(layoutXML.text); // Pass LayoutXML to it
-	}
+        drawPile = ConvertListCardsToListCardProspectors(deck.cards);
+    }
 
+    List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD) {
+        List<CardProspector> lCP = new List<CardProspector>();
+        CardProspector tCP;
+        foreach (Card tCD in lCD) {
+            tCP = tCD as CardProspector;
+            lCP.Add(tCP);
+        }
+        return (lCP);
+    }
 }
